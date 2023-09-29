8c6583ba3476ca04ec19f5e3a290b2d85513dc7b
AlexGlotov <90896181+AlexGlotov@users.noreply.github.com>
2023-09-29 18:55:35 +0300

    Update main.cpp

--- 1_diff/code/main.cpp
+++ 1_diff/code/main.cpp
@@ -9,7 +9,7 @@ int main() {
 	const std::array< double,16> step = { 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 1e-14, 1e-15 };
 	const double e = exp(1);
 
-	// Задание 1
+	// Task 1
 	
 	std::ofstream data0("data0.txt");
 
@@ -71,7 +71,7 @@ int main() {
 
 	data5.close();
 
-	 //Задание 2
+	 //Task 2
 
 	std::ofstream data01("data0-1.txt");
 
