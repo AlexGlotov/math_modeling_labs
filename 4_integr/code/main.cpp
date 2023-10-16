498d302689b22d022e57a3a8168d446c74e4eae9
AlexGlotov <90896181+AlexGlotov@users.noreply.github.com>
2023-10-16 20:08:09 +0300

    Update main.cpp

--- 4_integr/code/main.cpp
+++ 4_integr/code/main.cpp
@@ -16,18 +16,24 @@ int main()
 	//data2.precision(16);
 
 	const double I = 1 - std::cos(10);
-	/*for (double h = -7; h < 1; h += 0.001) {
+	for (double h = -7; h < 1; h += 0.001) {
 		double step = std::exp(h);
 
-		data2 << h << "		" << std::abs(I - integrate<decltype(func), double, 2>
+		/*data2 << h << "		" << std::abs(I - integrate<decltype(func), double, 2>
 			(func, start, end, nodes<double, 2>::p, nodes<double, 2>::w, step)) << std::endl;
 
 		data5 << h << "		" << std::abs(I - integrate<decltype(func), double, 5>
-			(func, start, end, nodes<double, 5>::p, nodes<double, 5>::w, step)) << std::endl;
+			(func, start, end, nodes<double, 5>::p, nodes<double, 5>::w, step)) << std::endl;*/
 
-	}*/
+		std::tuple<double, unsigned int> ret = integrate_<decltype(func), double, 5 >
+				(func, start, end, nodes<double, 5>::p, nodes<double, 5>::w, step);
+		double Int = std::get<0>(ret);
+		unsigned int K = std::get<1>(ret);
 
-	for (double err = -25; err < -1; err += 0.001) {
+		data3 << K << "		" << std::abs(I - Int) << std::endl;
+	}
+
+	/*for (double err = -25; err < -1; err += 0.001) {
 		/*data2 << err << "		" << std::abs(I - integrateRR<decltype(func), double, 2>
 			(func, start, end, nodes<double, 2>::p, nodes<double, 2>::w, std::exp(err))) << std::endl;
 
@@ -36,7 +42,7 @@ int main()
 
 		data3 << err << "		" << std::abs(I - integrateRR<decltype(func), double, 3>
 			(func, start, end, nodes<double, 3>::p, nodes<double, 3>::w, std::exp(err))) << std::endl;
-	}
+	}*/
 
 	//data2.close();
 	data3.close();
